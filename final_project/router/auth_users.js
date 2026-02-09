c582e9edc966b37d3011d6af4c67bc4fee1facab
itsSonchan <sonja.grund@icloud.com>
2026-02-09 15:44:57 -0500

    added login and authentication code

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,17 +6,36 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+//check is the username is valid
+let userToCheck=users.filter((user)=>{return user.username===username})
+if(userToCheck.length>0){
+    return true
+}
+else {return false}
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+//check if username and password match the one we have in records.
+let validUser=users.filter((user)=>user.username===username&&user.password===password)
+if (validUser>0){
+    return true
+}
+else{return false}
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username=req.body.username;
+  const password=req.body.password;
+  if (!username || !password){
+    return res.status(404).json({message: "User or password invalid or missing"})
+  }
+if (authenticatedUser(username,password)){
+    let accessToken=jwt.sign({data: password},"access",{expiresIn: 60*60})
+    req.session.authorization={accessToken, username}
+    return res.status(200).json({message: "User successfully logged in."})
+}
+else{
 });
 
 // Add a book review
